ca1d056478af8681b34e0b693234613f80314a4b
seksit39 <seksit39@outlook.com>
2017-02-19 11:42:58 +0700

    Add files via upload

---
+++ RX-ASK2BIT/RX-ASK2BIT.ino
@@ -0,0 +1,54 @@
+  int sum = 0;
+  int max = 0;
+  int prev = 0;
+  int check = false;
+  int output = -1;
+  int count = 0;
+  
+void setup() {
+  Serial.begin(115200);                   // set serial baudrate
+}
+
+void loop() {
+
+  int tmp = analogRead(A0);               // read signal from analog pin
+  
+  //Serial.println(tmp);
+  
+  if(tmp-prev>10 && check==false){        // check for rising signal
+    max = 0;
+    check = true;                         // change check status is true
+  }
+  
+  if(tmp>max){                            // for change max value
+    max=tmp;
+  }
+  
+  if(max-tmp > 10){                       // check for falling signal
+    if(check == true){
+      if(128<max && max<384){             // if max between 128 and 384
+        Serial.print("0 0 ");             
+        count++;
+      }        
+      else if(384<max && max<640){        // if max between 384 and 640
+        Serial.print("0 1 ");
+        count++;
+      }
+      else if(640<max && max<896){        // if max between 640 and 896
+        Serial.print("1 0 ");
+        count++;
+      }
+      else if(896<max && max<1024){       // if max between 896 and 1024
+        Serial.print("1 1 ");
+        count++;
+      }
+      if(count == 5){
+        Serial.println();
+        count = 0;
+      }
+    }
+    check = false;                        // change check status is false
+  }
+
+prev = tmp;                               // assign temp value to previous
+}
---
+++ TX-ASK2BIT/TX-ASK2BIT.ino
@@ -0,0 +1,34 @@
+#include <Wire.h>                             // library for I2C
+#include <Adafruit_MCP4725.h>                 // library for DAC
+
+  Adafruit_MCP4725 dac;                       // create DAC object
+  uint16_t input[4] = {0,0,0,0};              // for get input
+  const uint16_t sine[4] = {2048,4095,2048,0}; //at 1700 Hz
+
+void setup() {
+  Serial.begin(115200);                       // for set serial baudrate
+  dac.begin(0x62);                            // set DAC default ID
+  Serial.flush();                             // for clear buffer serial
+}
+
+void loop() {
+  
+    if(Serial.available() > 0){
+      
+        int in = Serial.parseInt();             // get Dec from Serial
+        for(int i=0 ; i<4 ; i++){               // 4 time of loop for a byte input
+          input[i] = in & 3;                    // and bit b0000 0011 and save to array
+          in >>= 2;                             // shift bit right 2 bit
+        }
+        //Serial.parseInt();
+        //for(int x=0 ; x<10 ; x++)
+        for(int k=3 ; k>=0 ; k--){              // 4 time loop for read data from LSB
+            //Serial.println(input[k]);         // for debug program
+            for(int i=0 ; i<4 ; i++){
+               dac.setVoltage(sine[i]*(input[k]+1)/4, false);   // calculate amplitude of sinewave
+            }
+         }
+      }   
+      dac.setVoltage(0, false);                 // for don't sent
+
+}
